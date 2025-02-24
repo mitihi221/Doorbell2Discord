b03b6ed8f12f71620ce9fab2669edf2c96f92791
mitihi221 <128671498+mitihi221@users.noreply.github.com>
2025-02-24 17:08:48 +0900

    Add files via upload

---
+++ secrets.py
@@ -0,0 +1,11 @@
+ssid = 'access_point_name'
+password = 'p@suwaaaaad0'
+
+dhcp = True
+static_ip = '0.0.0.0'
+static_mask = '0.0.0.0'
+static_gateway = '0.0.0.0'
+static_dns = '0.0.0.0'
+
+# Discord 通知用トークン(Webhook URL の数字から右の部分)
+discord_token = '0000000000000000000/********************************************************************'
